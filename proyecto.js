31e42c0eea0787f03410e21763676eb7c7274f80
rodrigonago <110421992+rodrigonago@users.noreply.github.com>
2022-08-03 08:24:41 -0500

    Create proyecto.js

---
+++ proyecto.js
@@ -0,0 +1,104 @@
+function CAMBIAR_GOLDENRETRIEVER () {
+let titulo_dogs = document.querySelector("#perros-seleccionado h2")
+titulo_dogs.innerHTML = "Golden retriever"
+let caja = document.querySelector("#perros-seleccionado img")
+caja.src = "goldenretriever2.jpg"
+let desc_perros = document.querySelector("#perros-seleccionado p")
+desc_perros.innerHTML = "El golden retriever o cobrador dorado es una raza de perro que se desarrolló alrededor de 1850 en el Reino Unido, más concretamente en Escocia. Con sus características de perro cobrador, sabueso, bloodhound y spaniel de agua, es un hábil perro de caza con aptitudes para el rastreo."
+}
+function CAMBIAR_HUSKYSIBERIANO () {
+let titulo_dogs = document.querySelector("#perros-seleccionado h2")
+titulo_dogs.innerHTML = "HUSKY SIBERIANO"
+let caja = document.querySelector("#perros-seleccionado img")
+caja.src = "HUSKY.jpg"
+let desc_perros = document.querySelector("#perros-seleccionado p")
+desc_perros.innerHTML = "El husky siberiano es una raza de perro de trabajo originaria del norte de Siberia. Este perro fue creado por la tribu Chukchi como perro de trabajo para tirar de los trineos a través de largas distancias "
+}
+function CAMBIAR_PASTORALEMAN () {
+    let titulo_dogs = document.querySelector("#perros-seleccionado h2")
+    titulo_dogs.innerHTML = "PASTOR ALEMAN"
+    let caja = document.querySelector("#perros-seleccionado img")
+    caja.src = "PASTOR ALEMAN.jpg"
+    let desc_perros = document.querySelector("#perros-seleccionado p")
+    desc_perros.innerHTML = "El pastor alemán u ovejero alemán es una raza canina que proviene de Alemania.​La raza es relativamente nueva, ya que su origen se remonta a 1899.​ Forman parte del grupo de pastoreo, ya que fueron perros desarrollados originalmente para reunir y vigilar ganado. "
+}
+function CAMBIAR_PUG () {
+    let titulo_dogs = document.querySelector("#perros-seleccionado h2")
+    titulo_dogs.innerHTML = "PUG"
+    let caja = document.querySelector("#perros-seleccionado img")
+    caja.src = "pug.jpg"
+    let desc_perros = document.querySelector("#perros-seleccionado p")
+    desc_perros.innerHTML = "El pug​ ​ es una raza de perro con origen histórico en China, pero con el patrocinio del Reino Unido. Se trata de un perro pequeño de tipo molosoide. En una encuesta realizada en 2012-2013 por la Federación Cinológica Internacional, el pug resultó ser una de las treinta razas más populares del mundo.​"
+}
+function CAMBIAR_CHOWCHOW () {
+    let titulo_dogs = document.querySelector("#perros-seleccionado h2")
+    titulo_dogs.innerHTML = "CHOW CHOW"
+    let caja = document.querySelector("#perros-seleccionado img")
+    caja.src = "CHOWCHOW.jpg"
+    let desc_perros = document.querySelector("#perros-seleccionado p")
+    desc_perros.innerHTML = "El chow chow —o sencillamente chow​— es una raza canina originaria del Norte de China.​​ Se cree que ​​ fue uno de los perros nativos utilizados como modelo para el perro de Foo, los guardianes tradicionales de piedra que se encuentran en frente de los templos y palacios budistas."
+}
+function CAMBIAR_DALMATA () {
+    let titulo_dogs = document.querySelector("#perros-seleccionado h2")
+    titulo_dogs.innerHTML = "DALMATA"
+    let caja = document.querySelector("#perros-seleccionado img")
+    caja.src = "Dálmata.jpg"
+    let desc_perros = document.querySelector("#perros-seleccionado p")
+    desc_perros.innerHTML = "El dálmata es una raza canina que debe su nombre a la histórica región de Dalmacia. Su característica principal es su singular pelaje moteado de color negro, hígado o limón. Al nacer, las crías carecen de manchas, las cuales van apareciendo por todo su cuerpo durante el primer año de vida."
+}
+function CAMBIAR_PASTORPERSA () {
+    let titulo_dogs = document.querySelector("#perros-seleccionado h2")
+    titulo_dogs.innerHTML = "PASTOR PERSA"
+    let caja = document.querySelector("#perros-seleccionado img")
+    caja.src = "PASTOR PERSA.png"
+    let desc_perros = document.querySelector("#perros-seleccionado p")
+    desc_perros.innerHTML = "El Pastor Persa es una raza de perro originaria de Irán, moloso tipo mastín, es probablemente el perro más grande del mundo, imponente e intimidante.​Es utilizado como perro guardián, perro pastor y perro guardián de ganado como todo mastín."
+}
+function CAMBIAR_PERROSALCHICHA () {
+    let titulo_dogs = document.querySelector("#perros-seleccionado h2")
+    titulo_dogs.innerHTML = "PERRO SALCHICHA"
+    let caja = document.querySelector("#perros-seleccionado img")
+    caja.src = "PERRO SALCHICHA.webp"
+    let desc_perros = document.querySelector("#perros-seleccionado p")
+    desc_perros.innerHTML = "perro salchicha, es una raza canina. Su peculiar fisonomía se debe a una mutación genética conocida como bassetismo, que dota a los ejemplares de unas extremidades cortas en relación con el tamaño del cuerpo."
+}
+function CAMBIAR_BULLDOG () {
+    let titulo_dogs = document.querySelector("#perros-seleccionado h2")
+    titulo_dogs.innerHTML = "BULLDOG "
+    let caja = document.querySelector("#perros-seleccionado img")
+    caja.src = "BULLDOG.jpg"
+    let desc_perros = document.querySelector("#perros-seleccionado p")
+    desc_perros.innerHTML ="Bulldog— es una raza canina originaria del Reino Unido. Su ancestro, conocido como el Antiguo Bulldog Inglés, fue utilizado en peleas de perros con toros hasta mediados del siglo XVII, aunque en 1835 esta práctica fue prohibida en Reino Unido."
+}
+function CAMBIAR_GRANDANES () {
+    let titulo_dogs = document.querySelector("#perros-seleccionado h2")
+    titulo_dogs.innerHTML = "GRAN DANES"
+    let caja = document.querySelector("#perros-seleccionado img")
+    caja.src = "gran-danes.jpg"
+    let desc_perros = document.querySelector("#perros-seleccionado p")
+    desc_perros.innerHTML ="El gran danés, dogo alemán o alano alemán es una raza canina conocida por su gran tamaño y personalidad delicada, considerado como el «Apolo entre todas las razas» por la Federación Cinológica Internacional."
+}
+function CAMBIAR_SHIBAINU () {
+    let titulo_dogs = document.querySelector("#perros-seleccionado h2")
+    titulo_dogs.innerHTML = "SHIBA INU"
+    let caja = document.querySelector("#perros-seleccionado img")
+    caja.src = "Japanese Shiba Inu2.webp"
+    let desc_perros = document.querySelector("#perros-seleccionado p")
+    desc_perros.innerHTML ="El shiba inu es el más pequeño de las seis originales y diferentes razas de perro de Japón. Es un perro pequeño y ágil que soporta muy bien el terreno montañoso, ya que fue originalmente criado para la caza. Es similar en apariencia al Akita Inu, aunque es mucho más pequeño en estatura."
+}
+function CAMBIAR_SANBERNARDO () {
+    let titulo_dogs = document.querySelector("#perros-seleccionado h2")
+    titulo_dogs.innerHTML = "SAN BERNARDO"
+    let caja = document.querySelector("#perros-seleccionado img")
+    caja.src = "san-bernardo-1-1024x670.jpg"
+    let desc_perros = document.querySelector("#perros-seleccionado p")
+    desc_perros.innerHTML ="El san bernardo es una raza de perro originaria de los Alpes suizos y del norte de Italia. Es el perro boyero más famoso junto con el mastín del Pirineo y es muy próximo al gran boyero suizo. Desciende del extinto mastín de los Alpes, que se mezcló con otras razas como el dogo del Tíbet, el terranova y el gran danés."
+}
+function CAMBIAR_SAMAYEDO () {
+    let titulo_dogs = document.querySelector("#perros-seleccionado h2")
+    titulo_dogs.innerHTML = "SAMAYEDO"
+    let caja = document.querySelector("#perros-seleccionado img")
+    caja.src = "samoyedo.jpg"
+    let desc_perros = document.querySelector("#perros-seleccionado p")
+    desc_perros.innerHTML = "El samoyedo es una raza de perro originaria de Rusia, que toma su nombre de los pueblos samoyedos del norte del país. Estos pueblos nómadas de pastores de renos criaron esta raza para ayudar con el pastoreo, tirar de trineos y para mantener a sus dueños calientes por las noches durmiendo a su lado. "
+}
